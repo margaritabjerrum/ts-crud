cf5fc751d67713128f31489bc55f8e0e56f507fe
margaritabjerrum <margarita.juskyte@gmail.com>
2023-01-17 17:46:11 +0200

    chore: browser changed to always be chrome

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,6 +2,14 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const webpackCommon = require('./webpack.config.common');
 
+const getBrowserName = () => {
+  switch (process.platform) {
+    case 'win32': return 'chrome';
+    case 'darwin': return 'Google Chrome'; 
+    default: return 'google-chrome'
+  }
+}
+
 module.exports = merge(webpackCommon, {
   mode: 'development',
   devtool: "source-map",
@@ -9,7 +17,13 @@ module.exports = merge(webpackCommon, {
     static: {
       directory: path.join(__dirname, 'public'),
     },
-    open: true,
+    open: {
+      target: ['http://localhost:3000/'],
+      app: {
+        name: getBrowserName(),
+        arguments: ['--new-window'],
+      },
+    },
     compress: true,
     port: 3000,
   },
