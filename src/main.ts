ebfe6355ba00551db6abc34e18deecca327cfff2
margaritabjerrum <margarita.juskyte@gmail.com>
2023-01-17 15:38:09 +0200

    feat: test code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import App from './components/app';
+import CarsCollection from './helpers/cars-collection';
+import cars from './data/cars';
+import brands from './data/brands';
+import models from './data/models';
 
 const app = new App('#root');
 app.initialize();
+
+const carsCollection = new CarsCollection({
+  cars,
+  brands,
+  models,
+});
+
+console.log(carsCollection.allCars);
