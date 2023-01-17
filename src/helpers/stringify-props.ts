6b016d6727cacb4766180b67094ba9d1d9b8d8be
margaritabjerrum <margarita.juskyte@gmail.com>
2023-01-17 17:45:21 +0200

    feat: helper for stringifying object

---
+++ src/helpers/stringify-props.ts
@@ -0,0 +1,19 @@
+type StringifiedObject<Type extends object> = {
+  [Key in keyof Type]: string
+};
+
+const stringifyProps = <Type extends object>(object: Type): StringifiedObject<Type> => {
+  const objectLikeArray = Object.entries(object);
+
+  const objectWithPropsStringified = objectLikeArray.reduce<Partial<StringifiedObject<Type>>>((
+    prevObj,
+    [key, value],
+  ) => ({
+    ...prevObj,
+    [key]: String(value),
+  }), {});
+
+  return objectWithPropsStringified as StringifiedObject<Type>;
+};
+
+export default stringifyProps;
